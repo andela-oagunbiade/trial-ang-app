e8fc5f2fcb402141ec1a15bcc92e7e08a3f87a4b
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2019-02-28 17:45:04 +0100

    Add service for getting mock heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-
+import { HeroService } from '../hero.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,16 +8,21 @@ import { HEROES } from './mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
 
   ngOnInit() {
+    this.getHeroes()
   }
 
 }
